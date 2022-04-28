695874a28c74978ee68f94f64c2327c95bf8ea73
kaizencodes <daniel.kaizencodes@gmail.com>
2022-04-28 10:40:42 +0100

    multi schema check

---
+++ avro/dsl/user.rb
@@ -0,0 +1,5 @@
+record :user do
+  required :name, :string
+  required :email, :string
+  optional :age, :int
+end
--- check_compatibility.rb
+++ check_compatibility.rb
@@ -4,20 +4,28 @@ require 'byebug'
 require 'avro_turf'
 require 'dotenv'
 require 'avro/builder'
-require 'avro_turf/messaging'
+require 'avro_turf/confluent_schema_registry'
 
 Dotenv.load
 
 base_path = File.expand_path(File.dirname(__FILE__))
 schema_path = File.join(base_path, 'avro', 'dsl')
 
-avro = AvroTurf::Messaging.new(
-  registry_url: ENV["REGISTRY_URL"],
-  user: ENV["API_KEY"],
-  password: ENV["API_SECRET"], 
-  schema_store: Avro::Builder::SchemaStore.new(path: schema_path))
-registry = avro.instance_variable_get(:@registry)
-store = avro.instance_variable_get(:@schema_store)
+store = Avro::Builder::SchemaStore.new(path: schema_path)
 
-valid = registry.compatible?("order_created", store.find("order_created"))
-abort("incompatible evolution") unless valid
+registry = AvroTurf::ConfluentSchemaRegistry.new(
+  ENV.fetch("REGISTRY_URL"),
+  user: ENV.fetch("API_KEY"),
+  password: ENV.fetch("API_SECRET"),
+)
+
+schema_names = Dir.entries(schema_path)
+  .select{ _1.match(/\w*.rb\z/) }
+  .map{ _1.gsub(/.rb/, "") }
+
+schema_names.each do |schema_name|
+  schema = store.find(schema_name)
+  if !registry.compatible?(schema_name, schema)
+    abort("incompatible evolution for schema: #{schema_name}") 
+  end
+end
